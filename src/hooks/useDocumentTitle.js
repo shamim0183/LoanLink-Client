f07b2a61f918bb2ee9a7e927b09dd1f29d2fcd0c
Shamim <shamim0183@gmail.com>
2025-12-06 18:39:41 +0600

    feat: add useDocumentTitle hook for dynamic page titles

---
+++ src/hooks/useDocumentTitle.js
@@ -0,0 +1,16 @@
+import { useEffect } from "react"
+
+const useDocumentTitle = (title) => {
+  useEffect(() => {
+    const prevTitle = document.title
+    document.title = title
+      ? `${title} | LoanLink`
+      : "LoanLink - Microloan Management System"
+
+    return () => {
+      document.title = prevTitle
+    }
+  }, [title])
+}
+
+export default useDocumentTitle
